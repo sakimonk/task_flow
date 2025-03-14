5131620be3ef8bdef900704435eb5d0ab90002f5
ouweijian <472083100@qq.com>
2025-03-14 13:05:16 +0800

    Fix compile error

--- task_flow_api/src/main/java/com/tf/task/flow/api/controller/TodoController.java
+++ task_flow_api/src/main/java/com/tf/task/flow/api/controller/TodoController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
--- task_flow_api/src/main/java/com/tf/task/flow/api/controller/UserController.java
+++ task_flow_api/src/main/java/com/tf/task/flow/api/controller/UserController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author ouweijian
